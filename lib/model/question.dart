4aa931746e1bee7fd8869d670b64c4cb44163a90
gmacgre <gmacgre@byu.edu>
2024-07-08 15:05:23 -0600

    Update Question to have Values

--- lib/model/question.dart
+++ lib/model/question.dart
@@ -1,11 +1,26 @@
 class Question {
-  const Question();
+  String question;
+  String answer;
+  String imageLink;
+  Question({
+    required this.question,
+    required this.answer,
+    required this.imageLink
+  });
 
   Map<String, dynamic> toJson() {
-    return {};
+    return {
+      'question': question,
+      'answer': answer,
+      'imageLink': imageLink,
+    };
   }
 
   factory Question.fromJson(Map<String, dynamic> json) {
-    return const Question();
+    return Question(
+      question: json['question'],
+      answer: json['answer'],
+      imageLink: json['imageLink']
+      );
   }
 }
